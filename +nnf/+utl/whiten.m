34d9b6e2df4e6641d57560f58d09fd4060f057f8
Nadith Pathirage <chathurdara@gmail.com>
2017-01-13 02:47:35 +0800

    [ENHANCE] whiten
    Signed-off-by: Nadith Pathirage <chathurdara@gmail.com>

--- +nnf/+utl/whiten.m
+++ +nnf/+utl/whiten.m
@@ -12,7 +12,20 @@ function [PX, W, m] = whiten(X, fudgefactor)
     [V, D0] = svd(Xc);
     D = D0 .^ 2;    
     
-    W = V * diag(sqrt(1./(diag(D) + fudgefactor)));
+    d = diag(D);
+    d(d<1e-10) = 0;
+       
+    en = find(~d);
+    if (~isempty(en) && en(1)-1 ~= 0)
+        en = en(1)-1;
+    else
+        en = numel(d);
+    end
+    
+    DD = diag(sqrt(1./(d + fudgefactor)));
+    DD = DD(:, 1:en);
+    
+    W = V * DD;
     PX = W' * Xc;
     
     % ZCA whitening
